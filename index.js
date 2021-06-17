03a9bc4a327f348067d03b7a4e7641a9e3b0cc60
anmolarora1711 <anmol1610140@akgec.ac.in>
2021-06-17 13:48:53 +0530

    add a controller and a rounter for home

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
         return;
     }
     console.log(`Server is running on the port: ${port}`);
-})
+});
