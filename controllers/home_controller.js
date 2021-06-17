d64181208b65da226b3fe41dc673052bc0a58532
anmolarora1711 <anmol1610140@akgec.ac.in>
2021-06-18 01:53:00 +0530

    setup static files access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codeial!</h1>')
+    
+    return res.render('home.ejs', {
+        title: "Home"
+    });
 }
 
 // module.exports.actionName = function(req, res){}
