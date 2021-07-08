9660ad84b8d568fbe1da4bc427f4f42c62a5ee8c
anmolarora1711 <anmol1610140@akgec.ac.in>
2021-07-08 20:59:10 +0530

    sign-in and sign-up pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,7 @@
 module.exports.home = function(req, res){
-    
-    return res.render('home.ejs', {
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
+    return res.render('home', {
         title: "Home"
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller.js');
+
 console.log('router loaded');
 
 router.get('/', homeController.home);
