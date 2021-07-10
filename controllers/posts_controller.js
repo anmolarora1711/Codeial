39ed79cad28647960fb822b1bdfe2991ce0b6aab
anmolarora1711 <anmol1610140@akgec.ac.in>
2021-07-11 00:50:12 +0530

    Display Posts and related User

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 
 module.exports.create = function(req, res){
-    Post.create.apply({
+    Post.create({
         content: req.body.content,
         user: req.user._id
     },function(err, post){
