16f3f4d5b5180cfd10a038852fe2ece81b544de2
Anmol Arora <anmol1610140@gmail.com>
2022-03-04 01:13:04 +0530

    Converting more code to Async Await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,32 +1,36 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 
-module.exports.create = function (req, res) {
-	Post.create(
-		{
+module.exports.create = async function (req, res) {
+	
+	try{
+		await Post.create({
 			content: req.body.content,
 			user: req.user._id,
-		},
-		function (err, post) {
-			if (err) {
-				console.log("Error in creating a post");
-				return;
-			}
-			return res.redirect("back");
-		}
-	);
+		});
+		
+		return res.redirect("back");
+	}catch(err){
+		console.log('Error', err);
+		return;
+	}
+
 };
 
-module.exports.destroy = function (req, res) {
-	Post.findById(req.params.id, function (err, post) {
-		// .id means converting the object id into string
+module.exports.destroy = async function (req, res) {
+
+	try{
+		const post = await Post.findById(req.params.id);
 		if (post.user == req.user.id) {
 			post.remove();
-			Comment.deleteMany({ post: req.params.id }, function (err) {
-				return res.redirect("back");
-			});
+			await Comment.deleteMany({ post: req.params.id });
+			return res.redirect('back');
 		} else {
-			return res.redirect("back");
+			return res.redirect('back');
 		}
-	});
+	}catch(err){
+		console.log('Error', err);
+		return;
+	}
+	
 };
